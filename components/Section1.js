79738b44149f3608fb48be585753d340349f61df
mhhasanmehedi <mmmehedi53@gmail.com>
2022-10-26 22:42:06 +0600

    add author component

--- components/Section1.js
+++ components/Section1.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
+import Author from "./_child/Author";
 
 const Section1 = () => {
+  const bg = {
+    background: "url('/images/banner.png') no-repeat",
+    backgroundPosition: "right",
+  };
   return (
-    <section className="py-16">
+    <section className="py-16" style={bg}>
       <div className="container mx-auto md:px-20">
         <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
         {Slide()}
@@ -20,7 +25,36 @@ const Slide = () => {
           <Image src="/images/img1.jpg" width={600} height={600} alt="Image" />
         </Link>
       </div>
-      <div className="info"></div>
+      <div className="info flex justify-center flex-col">
+        <div className="cat">
+          <Link href="/">
+            <span className="text-orange-600 hover:text-orange-800">
+              Business, Travel
+            </span>
+          </Link>
+          <Link href="/">
+            <span className="text-gray-600 hover:text-gray-800">
+              - July 3, 2022
+            </span>
+          </Link>
+        </div>
+
+        <div className="title">
+          <Link href="/">
+            <span className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">
+              Your most unhappy customers are greatest source of learning
+            </span>
+          </Link>
+
+          <p className="text-gray-500 py-3">
+            Even teh all-powerful Pointing has no control about the blind texts
+            it is an almost unorthographic life One day however a small line of
+            blind text by the name of Lorem ipsum decided to leave for the far
+            World of Grammar.
+          </p>
+          <Author />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ components/_child/Author.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Author = () => {
+  return (
+    <div className="author flex py-5">
+      <Image
+        src="/images/author/author1.jpg"
+        alt="Author"
+        width={60}
+        height={60}
+        className="rounded-full"
+      />
+      <div className="flex flex-col justify-center px-4">
+        <Link href="/">
+          <span className="text-md font-bold text-gray-800 hover:text-gray-600">
+            Flying High
+          </span>
+        </Link>
+        <span className="text-sm text-gray-500">CEO and Founder</span>
+      </div>
+    </div>
+  );
+};
+
+export default Author;
